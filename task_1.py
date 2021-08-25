3adbb75d339473d15176f549ac9202ef8b470dc8
Daniil <892971@gmail.com>
2021-08-25 07:18:57 +0200

    task_1 проще, чем казался изначально

---
+++ task_1.py
@@ -0,0 +1,14 @@
+def num_translate(el):
+    numbers = {'zero': 'ноль',
+               'one': 'один',
+               'two': 'два',
+               'three': 'три',
+               'four': 'четыре',
+               'five': 'пять',
+               'six': 'шесть',
+               'seven': 'семь',
+               'eight': 'восемь',
+               'nine': 'девять',
+               'ten': 'десять'}
+    return numbers.get(el)
+print(num_translate(input('Введите число на английском')))
